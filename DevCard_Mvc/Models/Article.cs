b382fe53fea316c03b177bc373c4e0c36bc18512
rezanabhani387 <rezanabhani387@gmail.com>
2022-02-15 19:22:20 +0330

    Articels Moved to view component

--- DevCard_Mvc/Models/Article.cs
+++ DevCard_Mvc/Models/Article.cs
@@ -5,6 +5,15 @@
         public long Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
+        public string Image { get; set; }
+
+        public Article(long id, string title, string description, string image)
+        {
+            Id = id;
+            Title = title;
+            Description = description;
+            Image = image;
+        }
     }
 
 }
---
+++ DevCard_Mvc/ViewComponents/ArticlesViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using DevCard_Mvc.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace DevCard_Mvc.ViewComponents
+{
+    public class ArticlesViewComponent : ViewComponent
+    {
+
+        public IViewComponentResult Invoke()
+        {
+            var articles = new List<Article>()
+            {
+                new Article(1,"آموزش Asp.net core mvc", "کاملترین پکیج آموزش asp.net core mvc به زبان فارسی","blog-post-thumb-card-1.jpg"),
+                new Article(2,"آموزش Asp.net core mvc", "کاملترین پکیج آموزش asp.net core mvc به زبان فارسی","blog-post-thumb-card-2.jpg"),
+                new Article(3,"آموزش Asp.net core mvc", "کاملترین پکیج آموزش asp.net core mvc به زبان فارسی","blog-post-thumb-card-3.jpg"),
+                new Article(4,"آموزش Asp.net core mvc", "کاملترین پکیج آموزش asp.net core mvc به زبان فارسی","blog-post-thumb-card-4.jpg"),
+            };
+            return View("_Articles",articles);
+        }
+
+    }
+}
