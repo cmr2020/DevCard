21b5ca1a89514e757da42071e19c22c265b86996
rezanabhani387 <rezanabhani387@gmail.com>
2022-02-15 18:45:46 +0330

    create viewcomponent Project

---
+++ DevCard_Mvc/ViewComponents/ProjectsViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using DevCard_Mvc.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace DevCard_Mvc.ViewComponents
+{
+    public class ProjectsViewComponent:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var projects = new List<Project>()
+            {
+                new Project(1,"تاکسی","درخواست آنلاین تاکسی برای سفر های درون شهری","project-1.jpg","Snapp"),
+                new Project(2,"زود فود","درخواست آنلاین غذا برای سراسر کشور","project-4.jpg","ZoodFood"),
+                new Project(3,"مدارس","سیستم مدیریت یکپارچه مدارس","project-5.jpg","MONOP"),
+                new Project(4,"فضاپیما","برنامه مدیریت فضا پیما های ناسا","project-7.jpg","NASA")
+            };
+            return View("_Projects",projects);
+        }
+    }
+}
